a2cfe24b0dda3207c5d5980bdb9203c010da05e8
emrekks <emrekks32@gmail.com>
2026-08-03 21:55:33 +0300

    fix: format hepsiburada contract tests

--- tests/MarketplaceHub.Adapters.ContractTests/F6AHepsiburadaContractTests.cs
+++ tests/MarketplaceHub.Adapters.ContractTests/F6AHepsiburadaContractTests.cs
@@ -1,5 +1,5 @@
-using System.Text.RegularExpressions;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using MarketplaceHub.Application;
 using MarketplaceHub.Domain;
 using MarketplaceHub.Infrastructure.Adapters.Hepsiburada;
