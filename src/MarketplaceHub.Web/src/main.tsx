b0e9bba89453547d33b553f9e857c0cb44704e6e
emrekks <emrekks32@gmail.com>
2026-08-30 03:49:56 +0300

    Centralize stylesheet entrypoint

--- src/MarketplaceHub.Web/src/main.tsx
+++ src/MarketplaceHub.Web/src/main.tsx
@@ -3,10 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { App } from './App'
-import './styles.css'
-import './splash.css'
-import './ravencia-theme.css'
-import './stitch-theme.css'
+import './app.css'
 
 const queryClient = new QueryClient({ defaultOptions: { queries: { retry: 1, staleTime: 60_000, refetchOnWindowFocus: false } } })
 createRoot(document.getElementById('root')!).render(<StrictMode><QueryClientProvider client={queryClient}><BrowserRouter><App /></BrowserRouter></QueryClientProvider></StrictMode>)
